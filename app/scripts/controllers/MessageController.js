7c68e0c424c635a1b69b854177d1ad33e4618126
David Jimenez Calvo <david.jimenez19@gmail.com>
2014-02-24 12:21:08 +0000

    Format

--- app/scripts/controllers/MessageController.js
+++ app/scripts/controllers/MessageController.js
@@ -1,59 +1,60 @@
 define([
-	'backbone',
-	'views/messages/ErrorMessageView',
-	'views/messages/LoadingMessageView'
+    'backbone',
+    'views/messages/ErrorMessageView',
+    'views/messages/LoadingMessageView'
 ],
-function( Backbone , ErrorMessageView,  LoadingMessageView) {
-    'use strict';
+    function (Backbone, ErrorMessageView, LoadingMessageView) {
+        'use strict';
 
-    var _getPleaseWaitDialog =  function() {
-        return $('#pleaseWaitDialog');
-    };
+        var _getPleaseWaitDialog = function () {
+            return $('#pleaseWaitDialog');
+        };
 
-	return Backbone.Marionette.Controller.extend({
+        return Backbone.Marionette.Controller.extend({
 
-        initialize: function(  ) {
-			console.log("initialize a MessageController");
+            initialize: function () {
+                console.log("initialize a MessageController");
 
-            TT.Communicator.mediator.on("message:showLoadingMask", this.showLoadingMask);
-            TT.Communicator.mediator.on("message:hideLoadingMask", this.hideLoadingMask);
-            TT.Communicator.mediator.on("message:showError", this.errorMessage);
-            TT.Communicator.mediator.on("message:hideError", this.hideErrorMessage);
-		},
-
-        errorMessage: function (title, message, okCallbackFn, okCallbackScope, okCallbackParams, cancelCallbackFn, cancelCallbackScope, cancelCallbackParams) {
-            console.log('Display error' + title + ' Msg:' + message);
-            //_getPleaseWaitDialog().addClass('hide');
-
-			var view = new ErrorMessageView( {attributes : {
-                title : title,
-                msg : message,
+                TT.Communicator.mediator.on("message:showLoadingMask", this.showLoadingMask);
+                TT.Communicator.mediator.on("message:hideLoadingMask", this.hideLoadingMask);
+                TT.Communicator.mediator.on("message:showError", this.errorMessage);
+                TT.Communicator.mediator.on("message:hideError", this.hideErrorMessage);
             },
-                okCallbackFn : okCallbackFn,
-                cancelCallbackFn : cancelCallbackFn
-            });
-			TT.App.modal.showModal(view);
-        },
 
-        hideErrorMessage: function () {
-            console.log('Hiding error popup');
-            TT.App.modal.hideModal();
-        },
+            errorMessage: function (title, message, okCallbackFn, okCallbackScope, okCallbackParams, cancelCallbackFn, cancelCallbackScope, cancelCallbackParams) {
+                console.log('Display error' + title + ' Msg:' + message);
+                //_getPleaseWaitDialog().addClass('hide');
 
-        showLoadingMask : function(title, message) {
-            console.log('Display error' + title + ' Msg:' + message);
+                var view = new ErrorMessageView({attributes: {
+                    title: title,
+                    msg: message,
+                },
+                    okCallbackFn: okCallbackFn,
+                    cancelCallbackFn: cancelCallbackFn
+                });
+                TT.App.modal.showModal(view);
+            },
 
-            var view = new LoadingMessageView( {attributes : {
-                title : title,
-                msg : message
-            	}
-			});
-            TT.App.modal.showModal(view);
-        },
+            hideErrorMessage: function () {
+                console.log('Hiding error popup');
+                TT.App.modal.hideModal();
+            },
 
-        hideLoadingMask : function() {
-			console.log('Hiding loading mask');
-            TT.App.modal.hideModal();
-        }
-	});
-});
+            showLoadingMask: function (title, message) {
+                console.log('Display error' + title + ' Msg:' + message);
+
+                var view = new LoadingMessageView({
+                    attributes: {
+                        title: title,
+                        msg: message
+                    }
+                });
+                TT.App.modal.showModal(view);
+            },
+
+            hideLoadingMask: function () {
+                console.log('Hiding loading mask');
+                TT.App.modal.hideModal();
+            }
+        });
+    });
