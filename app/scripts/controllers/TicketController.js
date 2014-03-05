5ce38fbe0be2483487b74338c13e746015270ddb
Jimenez, David <david.jimenez@tekcomms.com>
2014-03-05 11:07:47 +0000

    Move code to display ticket into the Ticket Module

--- app/scripts/controllers/TicketController.js
+++ app/scripts/controllers/TicketController.js
@@ -7,7 +7,7 @@ define([
     'hbs!tmpl/ticket',
     'hbs!tmpl/item/ticketView_tmpl'
 ],
-function( Backbone , Communicator, TicketEditView, TicketOpenView, TicketModel, ticketTemplate, ticketViewTmpl) {
+function( Backbone , Communicator) {
     'use strict';
 
     return Backbone.Marionette.Controller.extend({
@@ -18,89 +18,23 @@ function( Backbone , Communicator, TicketEditView, TicketOpenView, TicketModel, 
 
         displayTicketEdit : function (ticketId) {
 
-            var ticket = new TicketModel({
-                id:ticketId
-            });
-            ticket.fetch({
-                data : {
-                    'sourceApplication' : 'cqm',
-                    'dataMode' : 'SUMMARY'
-
-                },
-                success : function(ticketModel) {
-                    console.log('Retrieve ticket '+ ticketId);
-
-                    var ourTicketEdit = new TicketEditView({
-                        model : ticketModel
-                    });
-
-                    TT.App.layout.content.show(ourTicketEdit);
-
-                    TT.App.Communicator.mediator.trigger('message:hideLoadingMask');
-                },
-                error : function(a, b) {
-                    console.log(a, b);
-                }
+            require([ 'modules/Ticket' ], function(module) {
+                console.log('callback ticket module, displayTicketEdit' + module);
+                module.displayTicketEdit (ticketId);
             });
         },
         displayTicketOpen : function () {
 
-            var ticketModel = new TicketModel({
-                imsi:'123456789123456',
-                msisdn: '123456789123456',
-                startTime: '2013-11-09 00:00',
-                endTime: '2013-11-14 23:59',
-                title: 'title',
-                customerName: 'Bob McBob',
-                plan: 'Plan9',
-                businessName: 'Bobs Magical Wax Show',
-                avgBill: '10',
-                tariff: 'A',
-                type: 'My kind',
-                description: 'http://example.com',
-                sourceApplication: 'cqm'
-
+            require([ 'modules/Ticket' ], function(module) {
+                console.log('callback ticket module, displayTicketOpen' + module);
+                module.displayTicketOpen();
             });
-            var ourTicketOpen = new TicketOpenView({
-                model : ticketModel
-            });
-
-            TT.App.layout.content.show(ourTicketOpen);
-
-            TT.App.Communicator.mediator.trigger('message:hideLoadingMask');
         },
         displayTicketView : function(ticketId, Ticket) {
-            TT.App.Communicator.mediator.trigger('message:showLoadingMask');
 
-            var ticket = new Ticket({
-                id:ticketId
-            });
-            ticket.fetch({
-                data : {
-                    'sourceApplication' : 'cqm',
-                    'dataMode' : 'SUMMARY'
-
-                },
-                success : function(ticketModel) {
-                    console.log('Retrieve ticket '+ ticketId);
-
-                    var viewTicket = Backbone.Marionette.ItemView.extend({
-//                        tagName: '#content',
-                        template: ticketViewTmpl
-                    });
-                    var ourTicketView = new viewTicket({
-                        model : ticketModel
-                    });
-//                     TT.App.layout.regionManager.removeRegion('content');
-//                    TT.App.layout.regionManager.addRegion('content');
-
-                    TT.App.layout.content.show(ourTicketView);
-
-                    TT.App.Communicator.mediator.trigger('message:hideLoadingMask');
-                },
-                error : function(a, b) {
-                    console.log(a, b);
-                }
+            require([ 'modules/Ticket' ], function(module) {
+                console.log('callback ticket module, displayTicketView' + module);
+                module.displayTicketView(ticketId, Ticket);
             });
         },
         displayTicketList : function(){
---
+++ app/scripts/modules/Ticket.js
@@ -0,0 +1,109 @@
+define([
+    'backbone',
+    'communicator',
+    'application',
+    'views/item/ticketEdit',
+    'views/item/ticketOpen',
+    'models/Ticket',
+    'hbs!tmpl/ticket',
+    'hbs!tmpl/item/ticketView_tmpl'
+],
+
+    function (Backbone, Communicator, App, TicketEditView, TicketOpenView, TicketModel, ticketTemplate, ticketViewTmpl) {
+        'use strict';
+
+        var ticketModule = App.module('Ticket', function (TicketModule, MyApp, Backbone) {
+
+            TicketModule.displayTicketOpen = function () {
+
+                var ticketModel = new TicketModel({
+                    imsi:'123456789123456',
+                    msisdn: '123456789123456',
+                    startTime: '2013-11-09 00:00',
+                    endTime: '2013-11-14 23:59',
+                    title: 'title',
+                    customerName: 'Bob McBob',
+                    plan: 'Plan9',
+                    businessName: 'Bobs Magical Wax Show',
+                    avgBill: '10',
+                    tariff: 'A',
+                    type: 'My kind',
+                    description: 'http://example.com',
+                    sourceApplication: 'cqm'
+
+                });
+                var ourTicketOpen = new TicketOpenView({
+                    model : ticketModel
+                });
+
+                MyApp.layout.content.show(ourTicketOpen);
+
+                MyApp.Communicator.mediator.trigger('message:hideLoadingMask');
+            };
+
+            TicketModule.displayTicketView = function (ticketId, Ticket) {
+
+                MyApp.Communicator.mediator.trigger('message:showLoadingMask');
+
+                var ticket = new Ticket({
+                    id:ticketId
+                });
+                ticket.fetch({
+                    data : {
+                        'sourceApplication' : 'cqm',
+                        'dataMode' : 'SUMMARY'
+
+                    },
+                    success : function(ticketModel) {
+                        console.log('Retrieve ticket '+ ticketId);
+
+                        var ViewTicket = Backbone.Marionette.ItemView.extend({
+                            template: ticketViewTmpl
+                        });
+                        var ourTicketView = new ViewTicket({
+                            model : ticketModel
+                        });
+
+                        MyApp.layout.content.show(ourTicketView);
+
+                        MyApp.Communicator.mediator.trigger('message:hideLoadingMask');
+                    },
+                    error : function(a, b) {
+                        console.log(a, b);
+                    }
+                });
+            };
+
+            TicketModule.displayTicketEdit  = function(ticketId) {
+
+                var ticket = new TicketModel({
+                    id:ticketId
+                });
+                ticket.fetch({
+                    data : {
+                        'sourceApplication' : 'cqm',
+                        'dataMode' : 'SUMMARY'
+
+                    },
+                    success : function(ticketModel) {
+                        console.log('Retrieve ticket '+ ticketId);
+
+                        var ourTicketEdit = new TicketEditView({
+                            model : ticketModel
+                        });
+
+                        MyApp.layout.content.show(ourTicketEdit);
+
+                        MyApp.Communicator.mediator.trigger('message:hideLoadingMask');
+                    },
+                    error : function(a, b) {
+                        console.log(a, b);
+                    }
+                });
+            };
+
+        });
+
+        return ticketModule;
+    }
+);
